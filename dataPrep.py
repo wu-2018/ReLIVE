d8efafa94c14fe48b5f4e403d4e00b68a1c6afbc
Qinkai WU <2298300693@qq.com>
2018-09-26 16:16:47 +0800

    Package into a class

--- dataPrep.py
+++ dataPrep.py
@@ -5,48 +5,60 @@ import pandas as pd
 from collections import defaultdict
 
 dir_ = dirname(__file__)
-#### Read data
-pairData = pd.read_table(dir_+'/data/pairs.tsv')
-exprData = pd.read_table(dir_+'/data/expr.tsv')
 
-#### Get a pair dict in the form like 'ligand':{'r1','r2','r3',...}
-Ligands = pairData.Ligand
-Receptors = pairData.Receptor
+class DataPrep:
+    
+    def __init__(self, pD_file='/data/pairs.tsv', eD_file='/data/expr.tsv'):
+        #### Read data
+        self.pairData = pd.read_table(dir_ + pD_file)
+        self.exprData = pd.read_table(dir_ + eD_file)
 
-pairDict = defaultdict(list)
-for i in range(len(pairData)):
-    li = Ligands[i]
-    pairDict[li].append(Receptors[i])
-#print(pairDict)
+        #### Get a pair dict in the form like 'ligand':{'r1','r2','r3',...}
+        Ligands = self.pairData.Ligand
+        Receptors = self.pairData.Receptor
+        
+        self.pairDict = defaultdict(list)
+        for i in range(len(self.pairData)):
+            li = Ligands[i]
+            self.pairDict[li].append(Receptors[i])
+        #print(self.pairDict)
+         
+        #### Get gene set from the ligand-receptor data
+        self.Ligands = set(Ligands)
+        self.Receptors = set(Receptors)
+        self.All_genes = self.Ligands.union(self.Receptors)
+        #print(len(self.All_genes))
+        #print(pd.value_counts(exprData.index.isin(self.All_genes)))
 
-#### Get gene set from the ligand-receptor data
-Ligands = set(Ligands)
-Receptors = set(Receptors)
-All_genes = Ligands.union(Receptors)
-#print(len(All_genes))
-#print(pd.value_counts(exprData.index.isin(All_genes)))
+        self.input_gene_num, self.input_cell_num = self.exprData.shape
 
-input_gene_num, input_cell_num = exprData.shape
+        #### Filter out genes that are not in the ligand-receptor database
+        filteredExpr = self.exprData.loc[self.exprData.index.isin(self.All_genes), ]
 
-'''
-## generate a dict for originally input expression matrix
-#inputDict = dict(gene_index = range(gene_num), gene_name = exprData.index, 
-#                 cell_index = range(cell_num), cell_name = exprData.columns)
-'''
+        #### Discriminate between ligands and receptors
+        gene_type = np.where(filteredExpr.index.isin(self.Ligands), 'ligand', 'receptor')
+        ligandIndex = np.where(filteredExpr.index.isin(self.Ligands))[0]
+        receptorIndex = np.where(~filteredExpr.index.isin(self.Ligands))[0]
 
-#### Filter out genes that are not in the ligand-receptor database
-filteredExpr = exprData.loc[exprData.index.isin(All_genes), ]
+        f_gene_num, f_cell_num = filteredExpr.shape
+        filteredDict = dict(gene_index = list(range(f_gene_num)), gene_name = filteredExpr.index, 
+                            gene_type = gene_type, cell_index = list(range(f_cell_num)), 
+                            cell_name = filteredExpr.columns)
 
-#### Discriminate between ligands and receptors
-gene_type = np.where(filteredExpr.index.isin(Ligands), 'ligand', 'receptor')
-ligandIndex = np.where(filteredExpr.index.isin(Ligands))[0]
-receptorIndex = np.where(~filteredExpr.index.isin(Ligands))[0]
+        self.l_fExpr = filteredExpr.iloc[ligandIndex, ]
+        self.r_fExpr = filteredExpr.iloc[receptorIndex, ]
 
-f_gene_num, f_cell_num = filteredExpr.shape
-filteredDict = dict(gene_index = list(range(f_gene_num)), gene_name = filteredExpr.index, 
-                    gene_type = gene_type, cell_index = list(range(f_cell_num)), 
-                    cell_name = filteredExpr.columns)
+        L_index = ['L' + str(i) for i in range(self.l_fExpr.shape[0])]
+        C_index = ['C' + str(i) for i in range(self.l_fExpr.shape[1])]
+        R_index = ['R' + str(i) for i in range(self.r_fExpr.shape[0])]
+        index = L_index + C_index + R_index              
+        name =  list(self.l_fExpr.index) + list(self.l_fExpr.columns) + list(self.r_fExpr.index)
+        self.index2name = dict(zip(index, name))
+        self.name2index = dict(zip(name, index)) #!!! Be aware of duplicated names! Some preprocessing is needed!
 
-l_fExpr = filteredExpr.iloc[ligandIndex, ]
-r_fExpr = filteredExpr.iloc[receptorIndex, ]
+        self.l_fExpr.index = L_index
+        self.l_fExpr.columns = C_index
+        self.r_fExpr.index = R_index
+        self.r_fExpr.columns = C_index
+
 

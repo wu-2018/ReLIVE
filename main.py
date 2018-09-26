a2c7a4fcfdea4982d0d08141c19cb2869de2b7eb
Qinkai WU <2298300693@qq.com>
2018-09-26 15:46:15 +0800

    Add new panel for Cell-Cell plot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from os.path import dirname
-
 import dataPrep
 import tools
 import sidePlots
@@ -11,32 +10,42 @@ from bokeh.plotting import figure
 from bokeh.layouts import layout, widgetbox, column
 from bokeh.models import GraphRenderer, StaticLayoutProvider, Circle, MultiLine, HoverTool, TapTool, \
      BoxSelectTool, ColumnDataSource, Div, ImageURL, LabelSet, CustomJS, CustomJSHover
-from bokeh.models.widgets import RangeSlider
+from bokeh.models.widgets import RangeSlider, CheckboxGroup, Panel, Tabs, TextInput
 from bokeh.models.graphs import NodesAndLinkedEdges, EdgesAndLinkedNodes
 
+
+#### Transfer the data into Module `tools` for further calculation
+tools.data = dataPrep.DataPrep()
 #### Min and max value of the expression matrix
-L_max = max(dataPrep.l_fExpr.values.flatten())
-R_max = max(dataPrep.r_fExpr.values.flatten())
-L_min = min(dataPrep.l_fExpr.values.flatten())
-R_min = min(dataPrep.r_fExpr.values.flatten())
+L_max = max(tools.data.l_fExpr.values.flatten())
+R_max = max(tools.data.r_fExpr.values.flatten())
+L_min = min(tools.data.l_fExpr.values.flatten())
+R_min = min(tools.data.r_fExpr.values.flatten())
+
+#### x&y coordinates range for each part: cells(upper), ligands(left) and receptors(right) 
+node_ep = dict(c=[5,4,5,13], l=[3,0,-4,-10], r=[7,0,14,-10])
 
 #### Set interactive widgets
-l_RS_value = (74,139)
-r_RS_value = (15,60)
-l_RangeSlider = RangeSlider(title="Range for Ligand Expression", value=l_RS_value, start=int(L_min), end=int(L_max)+1, step=1)
-r_RangeSlider = RangeSlider(title="Range for Receptor Expression", value=r_RS_value, start=int(R_min), end=int(R_max)+1, step=1)
-
+#### Choose expression range of ligand and receptor genes respectively
+l_RS_value = (27,139)
+r_RS_value = (5,60)
+l_RangeSlider = RangeSlider(title="Range of Ligand Expression", value=l_RS_value, start=int(L_min), end=int(L_max)+1, step=1)
+r_RangeSlider = RangeSlider(title="Range of Receptor Expression", value=r_RS_value, start=int(R_min), end=int(R_max)+1, step=1)
+##
+checkbox_group = CheckboxGroup(labels=["op"], active=[], css_classes=['bk-cb'], name="bk_cb")
 #### Dense plot for Gene expression distribution
-l_d = dict(e_min=int(L_min), e_max=int(L_max)+1, value=l_RS_value, max_exprs=dataPrep.l_fExpr.max(axis=1))
-r_d = dict(e_min=int(R_min), e_max=int(R_max)+1, value=r_RS_value, max_exprs=dataPrep.r_fExpr.max(axis=1))
+l_d = dict(e_min=int(L_min), e_max=int(L_max)+1, value=l_RS_value, max_exprs=tools.data.l_fExpr.max(axis=1))
+r_d = dict(e_min=int(R_min), e_max=int(R_max)+1, value=r_RS_value, max_exprs=tools.data.r_fExpr.max(axis=1))
 
 s_plot_l = sidePlots.side_plot(l_d, l_RangeSlider)
 s_plot_r = sidePlots.side_plot(r_d, r_RangeSlider)
 
-#### Main Plot
-plot = figure(plot_width=800, plot_height=600, x_range=(-5,17), y_range=(-12,15), tools="")
-plot.title.text = "Ligand-Receptor Interaction"
+#### the Main Plot
+plot = figure(plot_width=1000, plot_height=600, x_range=(-5,17), y_range=(-12,15), tools="")
+#plot.title.text = "Ligand-Receptor Interaction"
 plot.min_border_left = 70
+plot.grid.visible = False
+plot.axis.visible = False
 graphA = GraphRenderer()
 
 nodeSource = graphA.node_renderer.data_source
@@ -73,76 +82,90 @@ cus_tt = CustomJSHover(code = '''
 ''')
 
 hover= HoverTool(renderers = [graphA], tooltips=TOOLTIPS, show_arrow=False, formatters=dict(display=cus_tt))
-
-JS_CODE ="""
-    var inds = cb_obj.selected.indices;
-    var data = cb_obj.data;
-    console.log('hi'); 
-    for (var i=0; i<inds.length; i++){
-        data['show_l'][inds[i]]=data['name'][inds[i];]
-    }
-    cb_obj.change.emit();
-    """
-#nodeSource.callback = CustomJS(code=JS_CODE)
-
-
-js2code='''
-        console.log("hi!!!!");
-
-        '''
-
-js2 = CustomJS(code=js2code)
-plot.add_tools(TapTool(callback=js2), hover, BoxSelectTool(callback=js2))
-
+plot.add_tools(hover, BoxSelectTool())
 graphA.selection_policy = NodesAndLinkedEdges()
 graphA.inspection_policy = EdgesAndLinkedNodes()
-
 plot.renderers.append(graphA)
 
 labels = LabelSet(x='x', y='y', text='name', text_color='#00cadb', x_offset=15, y_offset=1, source=nodeSource)
 plot.add_layout(labels)
+tab1 = Panel(child=plot, title="Cell-Gene Plot")
 
-####
-node_ep = dict(c=[5,4,5,13], l=[3,0,-4,-10], r=[7,0,14,-10])
+#### Cell-Cell plot
+r = 10
+cc_plot=figure(plot_width=720,plot_height=630,tools="",x_range=(-r-3.5,r+3.5),y_range=(-r-3.5,r+3.5),name="cc_plot")
+cc_plot.min_border_left = 70
+cc_plot.grid.visible = False
+cc_plot.axis.visible = False
+graphB=GraphRenderer()
+cc_nS = graphB.node_renderer.data_source
+cc_eS = graphB.edge_renderer.data_source
+
+graphB.node_renderer.glyph = Circle(size=16, fill_color='#a3a3cc')
+graphB.node_renderer.selection_glyph = Circle(fill_color='#ccccff')
+graphB.node_renderer.hover_glyph = Circle(fill_color='yellow')
+graphB.edge_renderer.glyph = MultiLine(line_color="#ffa200",line_width='width', line_alpha=0.7)#,line_join='miter')
+graphB.edge_renderer.selection_glyph = MultiLine(line_color='#55c2b7', line_width='width')
+graphB.edge_renderer.nonselection_glyph = MultiLine(line_color="#c98496", line_alpha=0.15, line_width='width')
+graphB.edge_renderer.hover_glyph = MultiLine(line_color='#2f92d7', line_alpha=0.85, line_width='width')
+
+cc_TOOLTIPS = """<div><p style="font-size:1.1em; font-weight:bold; margin:0px; padding:0px;">
+            <span style="color:#488ebc; font-size:1.1em; font-weight:bold; white-space:nowrap;">
+                @start_name ---- @end_name
+            </span></br></p><p>@tip</p></div>
+"""
+cc_hover= HoverTool(renderers = [graphB], tooltips=cc_TOOLTIPS , show_arrow=False)
+cc_plot.add_tools(cc_hover, TapTool())
+
+graphB.selection_policy = NodesAndLinkedEdges()
+graphB.inspection_policy = EdgesAndLinkedNodes()
+'''
+x_start=[i[0] for i in p_start]
+y_start=[i[1] for i in p_start]
+x_end=[i[0] for i in p_end]
+y_end=[i[1] for i in p_end]
+
+for i in range(len(x_start)):
+    p.add_layout(Arrow(end=OpenHead(line_color="firebrick", line_width=4,size=7),
+                   x_start=x_start[i], y_start=y_start[i], 
+                   x_end=x_end[i], y_end=y_end[i]))
+'''
+cc_labels = LabelSet(x='lx', y='ly', text='name', text_color='#00cadb', text_align='center', y_offset=-13, source=cc_nS)
+cc_plot.add_layout(cc_labels)
+cc_plot.renderers.append(graphB)
+tab2 = Panel(child=cc_plot, title="Cell-Cell Plot")
 
 ####
 def update():
-    nodeSource.data, edgeSource.data = \
-    tools.updatePlotData(dataPrep.l_fExpr, dataPrep.r_fExpr, dataPrep.pairDict, node_ep, l_RangeSlider.value, r_RangeSlider.value)
-    
-    nodes_pos = dict(zip(nodeSource.data['index'], zip(nodeSource.data['x'], nodeSource.data['y'])))
+    tick_all = False if checkbox_group.active==[] else True
+    nd, ed, cc_nd, cc_ed = \
+    tools.updatePlotData(node_ep=node_ep, tick_all=tick_all, 
+                         Ligand_Range=l_RangeSlider.value, Receptor_Range=r_RangeSlider.value)
+    nodes_pos = dict(zip(nd['index'], zip(nd['x'], nd['y'])))
     graphA.layout_provider = StaticLayoutProvider(graph_layout=nodes_pos)
     
+    cc_nodes_pos = dict(zip(cc_nd['index'], zip(cc_nd['x'], cc_nd['y'])))
+    graphB.layout_provider = StaticLayoutProvider(graph_layout=cc_nodes_pos)
+
+    nodeSource.data, edgeSource.data = nd, ed
+    cc_nS.data, cc_eS.data = cc_nd, cc_ed
+
+    #p_start=[nP[i] for i in cc_start]
+    #p_end=[nP[i] for i in cc_end]
     #nodeSource.data['show_l'] = ['']*len(nodeSource.data['index'])
     
     ## For debugging. Check if the source data are generated correctly.
     #pd.DataFrame(edgeSource.data).to_csv('edge.csv')
     #pd.DataFrame(nodeSource.data).to_csv('node.csv')
-    
 
 controls = [l_RangeSlider, r_RangeSlider]
 for control in controls:
     control.on_change('value', lambda attr, old, new: update())
+checkbox_group.on_change('active',lambda attr, old, new: update())
 
-#html template
-with open(dirname(__file__)+"/description.html") as f:
-    text = f.read()
-desc = Div(text = text, width = 800)
-
-sizing_mode = 'fixed'  # 'scale_width' also looks nice with this example
-
-#inputs = widgetbox(*controls, sizing_mode=sizing_mode)
-
-left_side = column(s_plot_l.show(), s_plot_r.show())
-
-#div layout/position in the final html
-l = layout([
-    [desc],
-    [left_side, plot],
-], sizing_mode=sizing_mode)
-
-update()  # initial load of the data
-
-curdoc().add_root(l)
-curdoc().title = "Ligand-Receptor Interaction"
+left_side = column(s_plot_l.show(), s_plot_r.show(), name='left_side')
+tabs = Tabs(tabs=[tab2, tab1], name="main_plots")
+update()
+for i in [left_side, tabs, checkbox_group]:
+    curdoc().add_root(i)
 
--- tools.py
+++ tools.py
@@ -1,5 +1,10 @@
+import itertools
+from collections import defaultdict, Counter
 import numpy as np
-from math import ceil
+import math
+
+data = None
+
 def scale_alpha(value, min_ = 0.1, max_ = 0.8):
     """Log2 minmax scale."""
     if len(value)==0:
@@ -26,6 +31,11 @@ def line_pos_generator(sx, sy, ex, ey, num, dec_digits = 3):
     py = np.linspace(sy,ey,num).round(dec_digits)
     return list(px), list(py)
 
+def circ_pos_generator(num, r):
+    circ = [i*2*math.pi/num for i in range(num)]
+    c_x = [r*math.cos(i) for i in circ]
+    c_y = [r*math.sin(i) for i in circ]
+    return c_x, c_y
 
 def bezier(start, end, control, steps):
     """Generate points in a bezier curve. 
@@ -71,89 +81,176 @@ def bezier_path_points(eD_start, eD_end, nD_x, nD_y, nD_index, step=20, bias=8):
     
     return xs, ys
 
-#### Set expression thresholds for ligand and receptor genes respectively
-##?! DEFAULT THRESHOLDS SHOULD BE BASED ON THE APPROPRIATE  NUMBER OF ELEMENTS PLOTED ON THE FIGURE
+#### A decorator  
+LR, RR = None, None
+def judge(func):
+    def inner(**kwargs):
+        global LR, RR, Pre_nodeData, Pre_edgeData, eD_p_Data, inPairEdges_index, cc_nodeData, cc_edgeData
+        if (LR, RR) != (kwargs['Ligand_Range'], kwargs['Receptor_Range']):
+            Pre_nodeData, Pre_edgeData, eD_p_Data, inPairEdges_index, cc_nodeData, cc_edgeData = func(**kwargs)
+        LR, RR = kwargs['Ligand_Range'], kwargs['Receptor_Range']
+        if kwargs['tick_all'] != True:
+            #choose part of the Data
+            nodeData_onlyP = dict()
+            edgeData_onlyP = dict() 
+            for i,v in Pre_edgeData.items():
+                edgeData_onlyP[i] = [v[ind] for ind in inPairEdges_index]
+            nodeData_onlyP['index'] = sorted(list(set(edgeData_onlyP['start'] + edgeData_onlyP['end'])))
+            nodeData_onlyP['name'] = [data.index2name[i] for i in nodeData_onlyP['index']]
+            
+            nodeData_onlyP, edgeData_onlyP = all_path(nodeData_onlyP, edgeData_onlyP, eD_p_Data, kwargs['node_ep'])
+            return nodeData_onlyP, edgeData_onlyP, cc_nodeData, cc_edgeData
+        else:
+            nodeData_all, edgeData_all = all_path(Pre_nodeData, Pre_edgeData, eD_p_Data, kwargs['node_ep'])
+            return nodeData_all, edgeData_all, cc_nodeData, cc_edgeData
 
+    return inner
 
-def updatePlotData(l_fExpr, r_fExpr, pairDict, node_ep, Ligand_Range=(50,70), Receptor_Range=(10,22)):
+@judge
+def updatePlotData(node_ep, tick_all, Ligand_Range, Receptor_Range):
     """When the ranges change, reselect the genes that will be shown on the plot, 
     also update the node positions, connections and their paths.
 
     Arguments:
-    l_fExpr, r_fExpr -- filtered ligand and receptor expression dataframe
     node_ep -- endpoint coordinates
     Ligand_Range, Receptor_Range -- Expression value range
     """
-    x_l = np.where((l_fExpr > ceil(Ligand_Range[0])) & (l_fExpr < ceil(Ligand_Range[1])))
-    x_r = np.where((r_fExpr > ceil(Receptor_Range[0])) & (r_fExpr < ceil(Receptor_Range[1])))
-
+    
     ## Ligands & Receptors that are within the range
-    in_Range_l = list(np.take(l_fExpr.index, list(set(x_l[0]))))
-    in_Range_r = list(np.take(r_fExpr.index, list(set(x_r[0]))))
-    ## Cells that has the ligand or receptor genes within the range
-    c_i = list(set(x_l[1]).union(set(x_r[1])))
-    in_Range_c = list(np.take(l_fExpr.columns, c_i))
+    x_l = np.where((data.l_fExpr > math.ceil(Ligand_Range[0])) & (data.l_fExpr < math.ceil(Ligand_Range[1])))
+    x_r = np.where((data.r_fExpr > math.ceil(Receptor_Range[0])) & (data.r_fExpr < math.ceil(Receptor_Range[1])))
 
+
+    ## Cells that has the ligand or receptor genes within the range
     ## Node index, use 'C','L','R' to distinguish cells, ligands and receptors
-    nD_index = ['C'+str(i) for i in c_i] + \
-               ['L'+str(i) for i in list(set(x_l[0]))] + \
-               ['R'+str(i) for i in list(set(x_r[0]))]
-    nD_name = in_Range_c + in_Range_l + in_Range_r
-    
-    ## Calculate nodes positions
-    cx,cy = line_pos_generator(*node_ep['c'], len(in_Range_c))
-    lx,ly = line_pos_generator(*node_ep['l'], len(in_Range_l))
-    rx,ry = line_pos_generator(*node_ep['r'], len(in_Range_r))
-    nD_x = cx+lx+rx
-    nD_y = cy+ly+ry
-    #### Put all information of nodes into a dict
-    nodeData = dict(index = nD_index, name = nD_name, x = nD_x, y = nD_y )
+    c_i = ['C'+str(i) for i in set(x_l[1]).union(set(x_r[1]))]
+    l_i = ['L'+str(i) for i in list(set(x_l[0]))]
+    r_i = ['R'+str(i) for i in list(set(x_r[0]))]
+    nD_index = c_i + l_i + r_i
+    nD_name = [data.index2name[i] for i in nD_index]
+    #### Put the information of nodes into a dict
+    Pre_nodeData = dict(index = nD_index, name = nD_name)
+
+
     ## Start or end of a connection/edge must correspond with node index 
-    eD_start = ['C' + str(x) for x in x_l[1]] + ['C' + str(x) for x in x_r[1]]
-    eD_end = ['L' + str(x) for x in x_l[0]] + ['R' + str(x) for x in x_r[0]]
-    
-    index2name = dict(zip(nD_index, nD_name))
-    name2index = dict(zip(nD_name, nD_index))
-    eD_sn = [index2name[i] for i in eD_start]
-    eD_en = [index2name[i] for i in eD_end]
-    
+    eD_start_l = ['C' + str(x) for x in x_l[1]]
+    eD_start_r = ['C' + str(x) for x in x_r[1]]
+    eD_end_l = ['L' + str(x) for x in x_l[0]]
+    eD_end_r = ['R' + str(x) for x in x_r[0]]
+    eD_start = eD_start_l + eD_start_r
+    eD_end = eD_end_l + eD_end_r
+
+    eD_sn = [data.index2name[i] for i in eD_start]
+    eD_en = [data.index2name[i] for i in eD_end]
+
     ## Get expression values
-    l_value = [l_fExpr.iloc[i] for i in zip(x_l[0], x_l[1])]
-    r_value = [r_fExpr.iloc[i] for i in zip(x_r[0], x_r[1])]
+    l_value = [data.l_fExpr.iloc[i] for i in zip(x_l[0], x_l[1])]
+    r_value = [data.r_fExpr.iloc[i] for i in zip(x_r[0], x_r[1])]
     eD_value = l_value + r_value
     ## L expressions are typically larger than R, so line alpha should be calculated respectively
     eD_scaled_alpha = scale_alpha(l_value) + scale_alpha(r_value)
-    ## Calculate path points for each edges
-    eD_xs, eD_ys = bezier_path_points(eD_start, eD_end, nD_x, nD_y, nD_index)
-    #### Put all information of connections/edges into a dict
-    edgeData = dict(start = eD_start, end = eD_end, start_name = eD_sn, end_name = eD_en,
-                    value = eD_value, scaled_alpha = eD_scaled_alpha, xs = eD_xs, ys = eD_ys)
-    #nodeData['show_l'] = ['*']*len(nD_index)
-    
-    edgeData['sqrt_scaled_alpha'] = list(np.sqrt(edgeData['scaled_alpha']))
-    
-    ## Add L-R pair edges information into the edgeData
-    lig = [index2name[i] for i in set(eD_end) if i[0]=='L']
-    rec = [index2name[i] for i in set(eD_end) if i[0]=='R']
-    l_r_edge = [(l,r) for l in lig for r in rec if (r in pairDict[l])]
-    eD_p_sn = list(zip(*l_r_edge))[0] if len(l_r_edge) != 0 else []
-    eD_p_en = list(zip(*l_r_edge))[1] if len(l_r_edge)!=0 else []
-    eD_p_start = [name2index[l] for l in eD_p_sn]
-    eD_p_end = [name2index[r] for r in eD_p_en]
-    #print(eD_p_start, '--->', eD_p_end)
-    eD_p_xs, eD_p_ys = bezier_path_points(eD_p_start, eD_p_end, nD_x, nD_y, nD_index)
+    eD_sqrt_scaled_alpha = list(np.sqrt(eD_scaled_alpha))    
+    #### Put the information of connections/edges into a dict
+    Pre_edgeData = dict(start = eD_start, end = eD_end, start_name = eD_sn, end_name = eD_en, value = eD_value, 
+                    scaled_alpha = eD_scaled_alpha, sqrt_scaled_alpha = eD_sqrt_scaled_alpha)
 
-    edgeData['display'] = [' ']*len(eD_start) + ['none']*len(eD_p_start) 
-    edgeData['start'].extend(eD_p_start)
-    edgeData['end'].extend(eD_p_end)
-    edgeData['start_name'].extend(eD_p_sn)
-    edgeData['end_name'].extend(eD_p_en)
+
+    ## Add L-R pair edges information into the edgeData
+    lig = [data.index2name[i] for i in set(eD_end_l)]
+    rec = [data.index2name[i] for i in set(eD_end_r)]
+    l_r_edge = [(l,r) for l in lig for r in rec if (r in data.pairDict[l])]
+    eD_p_sn = list(zip(*l_r_edge))[0] if len(l_r_edge) != 0 else []
+    eD_p_en = list(zip(*l_r_edge))[1] if len(l_r_edge) != 0 else []
+    eD_p_start = [data.name2index[l] for l in eD_p_sn]
+    eD_p_end = [data.name2index[r] for r in eD_p_en]
+    #print(eD_p_start, '--->', eD_p_end)
+    eD_p_Data = dict(start = eD_p_start, end = eD_p_end, start_name = eD_p_sn, end_name = eD_p_en)
+
+    inPairEdges_index = [i for i,v in enumerate(eD_end) if (v in set(eD_p_start + eD_p_end))]
+
+    cc_nodeData, cc_edgeData = cc_plot_data(eD_end_l, eD_start_l, eD_end_r, eD_start_r, eD_p_start, eD_p_end) 
+    return Pre_nodeData, Pre_edgeData, eD_p_Data, inPairEdges_index, cc_nodeData, cc_edgeData
+
+
+
+def all_path(nodeData, edgeData, eD_p_Data, node_ep):
+    #Calculate nodes positions
+    index_prefix = [i[0] for i in nodeData['index']]
+    node_index_num = Counter(index_prefix)
+    cx,cy = line_pos_generator(*node_ep['c'], node_index_num['C'])
+    lx,ly = line_pos_generator(*node_ep['l'], node_index_num['L'])
+    rx,ry = line_pos_generator(*node_ep['r'], node_index_num['R'])
+    nD_x = cx+lx+rx
+    nD_y = cy+ly+ry
+    nodeData['x'] = nD_x
+    nodeData['y'] = nD_y 
+
+    ## Path for edge
+    eD_xs, eD_ys = bezier_path_points(edgeData['start'], edgeData['end'], nD_x, nD_y, nodeData['index'])
+    edgeData['xs'] = eD_xs
+    edgeData['ys'] = eD_ys
+    ## Add LR-edges to the edgeData
+    eD_p_xs, eD_p_ys = bezier_path_points(eD_p_Data['start'], eD_p_Data['end'], nD_x, nD_y, nodeData['index'])
     edgeData['xs'].extend(eD_p_xs)
     edgeData['ys'].extend(eD_p_ys)
-    edgeData['scaled_alpha'].extend([0.5]*len(eD_p_start))
-    edgeData['value'].extend([None]*len(eD_p_start))
-    edgeData['sqrt_scaled_alpha'].extend([0.8]*len(eD_p_start))
-
-
+    LR_edge_num = len(eD_p_xs)
+    edgeData['display'] = [' ']*len(edgeData['start']) + ['none']*LR_edge_num
+    edgeData['sqrt_scaled_alpha'].extend([0.8]*LR_edge_num)
+    edgeData['scaled_alpha'].extend([0.5]*LR_edge_num)
+    edgeData['value'].extend([None]*LR_edge_num)
+    for k,v in eD_p_Data.items():
+        edgeData[k].extend(v)
+    #nodeData['show_l'] = ['*']*len(nD_index)
     return nodeData, edgeData
 
+
+
+def cc_plot_data(eD_end_l, eD_start_l, eD_end_r, eD_start_r, eD_p_start, eD_p_end):
+
+    ## To get something like {'L132': ['C7', 'C8', 'C16'], 'L188': ['C17']}
+    l_c_D = defaultdict(list)
+    r_c_D = defaultdict(list)
+    for a,b in zip(eD_end_l, eD_start_l):
+        l_c_D[a].append(b)
+    for a,b in zip(eD_end_r, eD_start_r):
+        r_c_D[a].append(b)
+
+    ## cc_D {('C16', 'C16'): ['L84-->R331','L95-->R331'], ('C18', 'C16'): ['L258-->R372']}
+    cc_D = defaultdict(list)
+    for l,r in zip(eD_p_start, eD_p_end):
+        for i in itertools.product(l_c_D[l], r_c_D[r]):
+            cc_D[i].append(data.index2name[l]+'-->'+data.index2name[r])
+    
+    cc_nD_index = list(set(i for ii in cc_D.keys() for i in ii))
+    cc_nD_name = [data.index2name[i] for i in cc_nD_index]
+    cc_x, cc_y = circ_pos_generator(len(cc_nD_index), 10)
+    cc_lx, cc_ly = circ_pos_generator(len(cc_nD_index), 10+1)
+    cc_nodeData = dict(index=cc_nD_index, name=cc_nD_name, x=cc_x, y=cc_y, lx=cc_lx, ly=cc_ly)
+
+    cc_nodes_pos = dict(zip(cc_nD_index, zip(cc_x, cc_y)))
+    
+    r_s = 1.5
+    r_prop_coef = (r_s+10)/10
+    xs_, ys_ = circ_pos_generator(30, r_s)
+    xs_ += xs_[:2]
+    ys_ += ys_[:2]
+    cc_start, cc_end, cc_w, cc_tip, cc_xs, cc_ys = [], [], [], [], [], []
+    for k,v in cc_D.items():
+        cc_start.append(k[0])
+        cc_end.append(k[1])
+        cc_w.append(len(v))
+        cc_tip.append(v)
+        if k[0] == k[1]:
+            x_pos, y_pos = cc_nodes_pos[k[0]]
+            cc_xs.append([i+x_pos*r_prop_coef for i in xs_])
+            cc_ys.append([i+y_pos*r_prop_coef for i in ys_])
+        else:
+            x_pos_s, y_pos_s = cc_nodes_pos[k[0]]
+            x_pos_e, y_pos_e = cc_nodes_pos[k[1]]
+            cc_xs.append([x_pos_s, x_pos_e])
+            cc_ys.append([y_pos_s, y_pos_e])
+    
+    cc_sn, cc_en = [data.index2name[i] for i in cc_start], [data.index2name[i] for i in cc_end]
+    cc_edgeData = dict(start=cc_start, end=cc_end, width=cc_w, start_name=cc_sn, end_name=cc_en,
+                       tip=cc_tip, xs=cc_xs, ys=cc_ys)
+    return cc_nodeData, cc_edgeData
